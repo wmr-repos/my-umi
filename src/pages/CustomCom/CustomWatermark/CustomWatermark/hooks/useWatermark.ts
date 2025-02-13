700fc2ce950928495aef225759bd6c6adb5fd1eb
zj_wmr <3177549194@qq.com>
2025-02-13 22:59:27 +0800

    feat: 水印2

--- src/pages/CustomCom/CustomWatermark/CustomWatermark/hooks/useWatermark.ts
+++ src/pages/CustomCom/CustomWatermark/CustomWatermark/hooks/useWatermark.ts
@@ -75,20 +75,65 @@ const getMergedOptions = (o: Partial<WatermarkOptions>) => {
 
 const getCanvasData = (
   options: Required<WatermarkOptions>,
-): Promise<{ width: number; height: number; base64Url: string }> => {}
+): Promise<{ width: number; height: number; base64Url: string }> => {
+  const { rotate, image, content, fontStyle, gap } = options
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')!
+  const ratio = window.devicePixelRatio
+
+  const configCanvas = (size: { width: number; height: number }) => {}
+
+  const drawText = () => {}
+
+  const drawImage = () => {}
+
+  return image ? drawImage() : drawText()
+}
 
 export default function useWatermark(params: WatermarkOptions) {
   const [options, setOptions] = useState(params || {})
 
+  const mergedOptions = getMergedOptions(options)
+  const { zIndex, gap } = mergedOptions
+  const watermarkDivRef = useRef<HTMLDivElement>()
 
-  const watermarkDivRef = useRef<HTMLDivElement>(null)
+  const container = mergedOptions.getContainer()
 
-  function drawWatermark() {}
+  function drawWatermark() {
+    if (!container) return
+
+    getCanvasData(mergedOptions).then(({ width, height, base64Url }) => {
+      const wmStyle = `
+        width:100%; 
+        height:100%; 
+        position:absolute;
+        top:0; 
+        left:0;
+        bottom:0;
+        right:0;
+        pointer-events:none;
+        z-index:${zIndex};
+        background-position:0 0;
+        background-repeat:repeat;
+        background-size:${gap[0] + width}px ${gap[1] + height} px;
+        background-image:url(${base64Url})
+        `
+
+      if (!watermarkDivRef.current) {
+        const div = document.createElement('div')
+        watermarkDivRef.current = div
+        container.append(div)
+        container.style.position = 'relative'
+      }
+
+      watermarkDivRef.current?.setAttribute('style', wmStyle)
+    })
+  }
 
   useEffect(() => {
     drawWatermark()
   }, [options])
-  
+
   return {
     generateWatermark: (newOptions: WatermarkOptions) => {
       setOptions(merge({}, options, newOptions))
